933a719359dcd2053a1691aeca1cc004c466e9cf
1156054203 <1156054203@qq.com>
2019-03-15 14:51:58 +0800

    spectra analysis

--- signature/spectra_analysis.sh
+++ signature/spectra_analysis.sh
@@ -60,7 +60,7 @@ wait
 #5 flank_sequence
 cat $outdir/${sample}.isec.anno.vcf|grep -v '^#'|awk -F '[\t|]' '{print\$1,\$2,\$27}'|awk 'BEGIN{OFS="\t"}{if(\$3=="1")print\$1,\$2-11,\$2-1,"forward",1,"+";else print\$1,\$2,\$2+10,"reverse",1,"-"}' >${outdir}/${sample}.5flank.bed
 #3 flank_sequence
-cat $outdir/${sample}.isec.anno.vcf|grep -v '^#'|awk -F '[\t|]' '{print\$1,\$2,\$27}'|awk 'BEGIN{OFS="\t"}{if(\$3=="1")print\$1,\$2,\$2+10,"reverse",1,"-";else print\$1,\$2-11,\$2-1,"forward",1,"+"}' >${outdir}/${sample}.3flank.bed
+cat $outdir/${sample}.isec.anno.vcf|grep -v '^#'|awk -F '[\t|]' '{print\$1,\$2,\$27}'|awk 'BEGIN{OFS="\t"}{if(\$3=="1")print\$1,\$2,\$2+10,"forward",1,"+";else print\$1,\$2-11,\$2-1,"reverse",1,"-"}' >${outdir}/${sample}.3flank.bed
 
 wait
 /online/software/bedtools-2.25.0/bin/bedtools getfasta -fi $hg38_fa -bed ${outdir}/${sample}.5flank.bed -tab -s -fo $outdir/${sample}.5flank.seq
