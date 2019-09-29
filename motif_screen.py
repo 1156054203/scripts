5020455eb3389d60e881b7ebb366e7faf587aa5e
1156054203 <1156054203@qq.com>
2019-09-29 08:53:39 +0800

    regex match screen motif of seq

---
+++ motif_screen.py
@@ -0,0 +1,33 @@
+#!/bin/env python
+##chenyl
+##screen motif seq of DNA_AR.fa
+
+#from Bio import motifs
+from Bio import SeqIO
+import re,sys
+
+class motif_screen:
+    def __init__(self,fasta):
+        #self.motif=motif
+        self.fasta=fasta
+ 
+    def getseq(self):
+        record=SeqIO.read(self.fasta,'fasta')
+        ntseq=record.seq
+        aaseq=record.seq.translate()
+        pattern = re.compile(r'[AVP].[ST]{3}')
+        allmatch = re.findall(pattern,str(aaseq))
+        mitseq=[]
+        for i in allmatch:
+            start=re.search(i,str(aaseq)).span()[0]*3
+            end=re.search(i,str(aaseq)).span()[1]*3
+            mitseq.append(('start: %s' % start,'end: %s' % end,'motif: %s' % str(ntseq[start:end])))
+        mitseq.append(('protein: %s' % str(aaseq)))
+        return mitseq
+
+if __name__=='__main__':
+    if len(sys.argv) < 2:
+        print('Usage:\n     python  %s  DNA_AR.fa' % sys.argv[0])
+        sys.exit()
+    motif=motif_screen(sys.argv[1])            
+    print(motif.getseq())
