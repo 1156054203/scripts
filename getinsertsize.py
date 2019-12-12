d66f76199ef441c66d5dd08f0372c2144a527ab5
1156054203 <1156054203@qq.com>
2019-12-12 12:24:24 +0800

    stat insert size and Standard Deviation from read length file

--- getinsertsize.py
+++ getinsertsize.py
@@ -1,13 +1,20 @@
 #!/bin/env python
-# stat insert size and Standard Deviation from *.flagstat.txt
-# *.flagstat.txt file -- first column:number second column:read length
+#stat insert size and Standard Deviation from *fragmentLength.txt
+#usage: python fragmentLength.txt [1000] 
+#out: fragmentLength.rm1000.txt
+#fragmentLength.txt: two columns,frist is num, second is length
+#[1000]: how long segments to keep
+
 from __future__ import division
-import sys,os,math
+import sys,os,math,random
+
+
 
 def getinsertsize(infile,flag):
    file=open(infile,'r')
    name=os.path.split(infile)[1].split('.')[0]
-   out=open(name+'insertsize.txt','w')
+   out=open(name+'.rm'+base+'.insertsize.txt','w')
+   prop=random.uniform(0.08,0.12)
    readlist=[]
    for line in file:
       line=line.strip()
@@ -23,20 +30,23 @@ def getinsertsize(infile,flag):
             continue
 
    average = sum(readlist)/len(readlist)
+   propstd=average*prop
    cusum = 0
    for value in readlist:
       cusum += (value-average)**2
 
-   std = math.sqrt(cusum/len(readlist))
-   out.write(str(average)+'\t'+str(std))
-   return average,std
+   std = round(math.sqrt(cusum/len(readlist)))
+   out.write(name+'\t'+str(round(average))+'\t'+str(round(std))+'\t'+str(round(propstd))+'\n')
+   return round(average),round(std),round(propstd)
 
 if __name__=="__main__":
    if len(sys.argv) == 3:
       flag='true'
       max=sys.argv[2]
+      base=sys.argv[2]
    elif len(sys.argv) == 2:
       flag='false'
+      base='0'
    else:
       sys.exit()
    getinsertsize(sys.argv[1],flag)
