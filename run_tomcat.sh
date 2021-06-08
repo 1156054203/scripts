858fbd6ee861e288350420e2b536e6c1de03a089
1156054203 <1156054203@qq.com>
2021-06-08 16:48:59 +0800

    run tomcat

--- run_tomcat.sh
+++ run_tomcat.sh
@@ -3,15 +3,16 @@
 usage="Usage:\n\tbash $0 -r/-s\n
       \noptions:\n\t -r\trun  tomcat
       \n          \t -s\tstop tomcat"
+stfdir=/home/chenyl/software/tomcat/bin
 
 if [ $# -ne 1 ];then
    echo -e $usage
    exit 1
 elif [[ $1 == '-r' ]];then
-   bash /home/chenyl/tomcat/bin/startup.sh
+   bash $stfdir/startup.sh
    echo -e "\ntomcat is runnig"
 elif [[ $1 == '-s' ]];then
-   bash /home/chenyl/tomcat/bin/shutdown.sh
+   bash $stfdir/shutdown.sh
    echo -e "\ntomcat has been shut down"
 else
    echo -e $usage
