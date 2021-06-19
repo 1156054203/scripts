8dac79df9110c3ec1198df532e10faedc5fc0968
1156054203 <1156054203@qq.com>
2021-06-19 21:35:47 +0800

    run tomcat script

--- run_tomcat.sh
+++ run_tomcat.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 
-usage="Usage:\n\tbash $0 -r/-s\n
+usage="Usage:\n\tbash $0 -r/-s [idxdir]\n
       \noptions:\n\t -r\trun  tomcat
-      \n          \t -s\tstop tomcat"
-stfdir=/home/chenyl/software/tomcat/bin
+      \n          \t -s\tstop tomcat
+      \n          \t idxdir\tthe absolute path of directory for index.html"
 
-if [ $# -ne 1 ];then
-   echo -e $usage
-   exit 1
-elif [[ $1 == '-r' ]];then
-   bash $stfdir/startup.sh
-   echo -e "\ntomcat is runnig"
-elif [[ $1 == '-s' ]];then
-   bash $stfdir/shutdown.sh
-   echo -e "\ntomcat has been shut down"
+if [ $# -eq 2 ];then
+   sed -i "s#docBase=\(".*"\) #docBase=\"$2\" #g" tomcat/conf/server.xml
+   if [[ $1 == '-r' ]];then
+      bash /home/chenyl/tomcat/bin/startup.sh
+      echo -e "\ntomcat is runnig"
+   elif [[ $1 == '-s' ]];then
+      bash /home/chenyl/tomcat/bin/shutdown.sh
+      echo -e "\ntomcat has been shut down"
+   else
+      echo -e $usage
+      exit 1
+   fi
+elif [ $# -eq 1 ];then
+   if [[ $1 == '-r' ]];then
+      bash /home/chenyl/tomcat/bin/startup.sh
+      echo -e "\ntomcat is runnig"
+   elif [[ $1 == '-s' ]];then
+      bash /home/chenyl/tomcat/bin/shutdown.sh
+      echo -e "\ntomcat has been shut down"
+   else
+      echo -e $usage
+      exit 1
+   fi
 else
    echo -e $usage
    exit 1
